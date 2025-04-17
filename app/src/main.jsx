2675279d5112b29968cecaf58e327246242267c8
Sara Burns Gibson <saraburnsgibson@gmail.com>
2025-04-16 22:05:19 -0400

    change

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './tinytowns.jsx';
+import { createRoot } from 'react-dom/client';
+import { TownBoard } from './tinytowns'; // 👈 Make sure tinytowns exports TownBoard, not App
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(React.createElement(App));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <TownBoard />
+  </React.StrictMode>
+);
